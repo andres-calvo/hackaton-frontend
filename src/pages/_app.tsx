3577a2d155e280b4e5d648549264b91bfadc5453
Jose Waldo <josferw@gmail.com>
2023-11-04 11:11:07 -0500

    Chore: configuraciones en el proyecto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Manrope } from 'next/font/google'
+import { Manrope } from "next/font/google";
 
 const font = Manrope({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={` ${font.className}`}>
+    <main className={`${font.className}`}>
       <Component {...pageProps} />
     </main>
   );
