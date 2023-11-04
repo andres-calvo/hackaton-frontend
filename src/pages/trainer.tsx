681f57fe8f8f62184fc496c6d5277c3f403aa033
Andres Calvo <calvo.andres.tech@gmail.com>
2023-11-04 14:53:55 -0500

    Trainer

--- src/pages/trainer.tsx
+++ src/pages/trainer.tsx
@@ -1,9 +1,8 @@
-import Header from "@/components/Header";
 import { useQuery } from "@tanstack/react-query";
+import ReactMarkdown from "react-markdown";
 import axios from "axios";
-
 const TrainerPage = () => {
-  const { data, isFetching } = useQuery({
+  const { data: resp, isFetching } = useQuery({
     queryKey: ["trainerData"],
     queryFn: () => {
       return axios.get("/api/ai");
@@ -12,7 +11,23 @@ const TrainerPage = () => {
 
   return (
     <section className="flex flex-col h-full items-center">
-      <Header />
+      <header className="flex justify-between max-w-3xl w-full">
+        {/* Logo */}
+        {/* Trainer SVG */}
+      </header>
+      {isFetching ? (
+        <div role="status" className="max-w-sm animate-pulse">
+          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
+          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+          <span className="sr-only">Loading...</span>
+        </div>
+      ) : (
+        <ReactMarkdown>{resp?.data}</ReactMarkdown>
+      )}
       <div></div>
     </section>
   );
